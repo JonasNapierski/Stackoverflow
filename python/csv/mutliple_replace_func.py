aa8660bc8f54d5c17232cf6fe9f82e3a726c9407
Jonas Napierski <pudeljoni@gmail.com>
2022-10-31 22:56:31 +0100

    fix small typo

--- python/csv/mutliple_replace_func.py
+++ python/csv/mutliple_replace_func.py
@@ -1,4 +1,5 @@
 import csv
+from tokenize import Octnumber
 import requests
 from bs4 import BeautifulSoup
 from csv import QUOTE_NONE
@@ -11,34 +12,27 @@ soup = BeautifulSoup(response,'lxml')
 
 tbody=soup.find('table', id='main_table_countries_today').find('tbody').find_all('tr')[100:110]
 
-replacement_pattern = {
+replacement = {
     ",": "",
     "N/A": "0",
-    "\n": ""
+    "\n": "",
+    " ": ""
 }
 
+def cleanup(webcontent, indecies):
+    out = []
+    for index in indecies:
+        content = webcontent.find_all('td')[index].text
+        for k in [*replacement]:
+            content = content.replace(k,replacement[k])
+        out.append(content)
+    return out
+     
 with open('corona1.csv','w') as csv_file:
     csv_writer = writer(csv_file, escapechar=' ', quoting=csv.QUOTE_NONE)
     csv_writer.writerow(['countries','total_cases','total_deaths','total_recovered','active_cases','total_cases_in_1m','deaths_in_1m','population'])
 
-
-
     for value in tbody:
-            countries = value.find_all('td')[1].text.replace(",", "").strip()
-            total_cases= value.find_all('td')[2].text.replace(",", "").strip()
-            total_deaths=value.find_all('td')[4].text.replace(",", "").strip()
-            
-            total_recovered_raw=value.find_all('td')[6].text
-            for _key in [*replacement_pattern]:
-                total_recovered_raw.replace(_key, replacement_pattern[_key])
-            total_recovered = total_recovered_raw.strip()
-            print(total_recovered_raw)
-            active_cases=value.find_all('td')[8].text.replace(",", "").strip()
-            total_cases_in_1m=value.find_all('td')[10].text.replace(",", "").strip()
-            deaths_in_1m=value.find_all('td')[11].text.replace(",", "").strip()
-            population=value.find_all('td')[14].text.replace(",", "").strip()
-
-
-            csv_writer.writerow([countries,total_cases,total_deaths,total_recovered,active_cases,total_cases_in_1m,deaths_in_1m,population])
-
+        csv_writer.writerow(cleanup(value, [1,2,4,6,8,10,11,14]))
+        
 
